0f6e5002136fc351913323b52d1ff28221089179
Maggie McCarthy <108485831+maggiemcc02@users.noreply.github.com>
2026-07-03 14:23:31 +0100

    Most recent DWR for Matt code

---
+++ code/MattsAlgorithm_for_Maxwell/MostRecentFixes_DWRforMatt.py
@@ -0,0 +1,549 @@
+from firedrake import *
+import numpy as np
+import matplotlib
+matplotlib.use("PDF")
+import matplotlib.pyplot as plt
+import os
+
+
+
+# CHANGES SINCE UMBERTO
+######################################################################################
+
+# Made sure the least squares solution is normalized
+
+# General clean up of code (took out some useless stuff)
+
+# Change uh_lift to uh in the residual assembly   
+
+# Swapped coordinates in K matrix -> Kij = <basis_j, basis_i>. I think this matters for complex numbers (conjugate in inner produc)
+
+# Added a multiplicity comparison plot 
+
+
+# QUESTIONS
+######################################################################################
+
+# Kij indexing
+
+# Should I be using uh or uh_lift
+
+# eigenfunction(0)[0] does what? should I be using it?
+
+
+
+
+
+# EIGENPROBLEM SETUP (PRIMAL AND ENRICHED)
+######################################################################################
+
+
+
+
+# Set a triangular mesh like in Matt's example (Fig 3.22)
+N = 128
+mesh = SquareMesh(N, N, pi, quadrilateral=False)
+
+
+
+# Decide if you want edge elements or Lagrange elements to discretize the space for E
+hcurl = False
+if hcurl:
+    V0 = FunctionSpace(mesh, "N1curl", 2)
+    V0p = FunctionSpace(mesh, "N1curl", 3) # enriched space
+else:
+    V0 = VectorFunctionSpace(mesh, "CG", 1, dim=2)
+    V0p = VectorFunctionSpace(mesh, "CG", 2, dim=2) # enriched space
+
+# The space for H is CG1
+V1 = FunctionSpace(mesh, "CG", 1)
+V1p = FunctionSpace(mesh, "CG", 2) # enriched space
+
+# We need a mixed function space here: (space for E) x (space for H)
+Z = MixedFunctionSpace([V0, V1])
+Zp = MixedFunctionSpace([V0p, V1p]) # enriched space
+
+# Set zero tangential trace conditions for E:
+# Edge elements only force continuity in tangential component so we just set zero conditions on boundary.
+# If using Lagrange elements, we need so explicily set the tangential trace to zero.
+if hcurl:
+    bc = [DirichletBC(Z.sub(0), Constant((0, 0)), "on_boundary")]
+    bcp = [DirichletBC(Zp.sub(0), Constant((0, 0)), "on_boundary")]
+else:
+    bc = [DirichletBC(Z.sub(0).sub(0), 0, (3, 4)),
+          DirichletBC(Z.sub(0).sub(1), 0, (1, 2))]
+    bcp = [DirichletBC(Zp.sub(0).sub(0), 0, (3, 4)),
+          DirichletBC(Zp.sub(0).sub(1), 0, (1, 2))]
+
+
+# Scalar rot
+def rot_s(E):
+    return E[1].dx(0) - E[0].dx(1)
+
+# Vector rot
+def rot_v(H):
+    return as_vector([H.dx(1), -H.dx(0)])
+
+# Set the complex unit 
+complex_unit = Constant(1j)
+
+
+
+
+
+
+# SET THE PRIMAL EIGENPROBLEM AND SOLVER
+######################################################################################
+
+
+
+
+# Test and trial functions
+u = TrialFunction(Z)
+(E, H) = split(u)
+v = TestFunction(Z)
+(F, G) = split(v)
+
+# Placeholder for eigenvalue
+z = Constant(0)
+
+# Set Ln(z) = <(A-zI)u, (A-zI)v>
+a = (
+      # A^* M^{-1} A terms
+      inner(rot_s(E), rot_s(F))*dx
+    + inner(rot_v(H), rot_v(G))*dx
+      # -2 z A terms
+    - 2 * z * inner(complex_unit*rot_v(H), F)*dx
+    + 2 * z * inner(complex_unit*rot_s(E), G)*dx
+      # |z|^2 M terms
+    + conj(z) * z * inner(E, F)*dx
+    + conj(z) * z * inner(H, G)*dx
+    )
+
+# Set Gn = <u, v>
+b = inner(E, F)*dx + inner(H, G)*dx
+
+
+# Set the generalized eigenproblem Ln(z)x = lambda Gn x on restructed space
+primal_problem = LinearEigenproblem(a, b, bcs=bc, restrict=True)
+
+# Solver parameters and subsequent solver
+sp = {"eps_gen_hermitian": None,  # solver parameters, passed to SLEPc
+      "eps_type": "krylovschur",
+      "eps_tol": 1e-8,
+      "eps_monitor": None,
+      "eps_smallest_magnitude": None,
+      #"eps_view": None,  # uncomment to see the solver
+      "eps_target": 0,
+      "eps_target_real": None,
+      "st_type": "sinvert",
+      }
+primal_solver = LinearEigensolver(primal_problem, n_evals=5, solver_parameters=sp)
+
+
+
+
+
+
+
+# SET THE ENRICHED EIGENPROBLEM AND SOLVER
+######################################################################################
+
+
+
+
+
+# Test and trial functions
+up = TrialFunction(Zp)
+(Ep, Hp) = split(up)
+vp = TestFunction(Zp)
+(Fp, Gp) = split(vp)
+
+# Set Ln(z) = <(A-zI)u, (A-zI)v>
+ap = (
+      # A^* M^{-1} A terms
+      inner(rot_s(Ep), rot_s(Fp))*dx
+    + inner(rot_v(Hp), rot_v(Gp))*dx
+      # -2 z A terms
+    - 2 * z * inner(complex_unit*rot_v(Hp), Fp)*dx
+    + 2 * z * inner(complex_unit*rot_s(Ep), Gp)*dx
+      # |z|^2 M terms
+    + conj(z) * z * inner(Ep, Fp)*dx
+    + conj(z) * z * inner(Hp, Gp)*dx
+    )
+
+# Set Gn = <u, v>
+bp = inner(Ep, Fp)*dx + inner(Hp, Gp)*dx
+
+
+# Set the generalized eigenproblem Ln(z)x = lambda Gn x on restructed space
+enriched_problem = LinearEigenproblem(ap, bp, bcs=bcp, restrict=True)
+
+
+# solver
+enriched_solver = LinearEigensolver(enriched_problem, n_evals=10, solver_parameters=sp)
+
+
+
+
+
+
+# COMPUTE PHI AT EACH POINT IN GRID(n). ALSO COMPUTE THE DWR ERROR ESTIMATE
+######################################################################################
+
+
+
+
+
+# Set Grid(n) and empty lists for phi and error values
+h = 0.02
+n = 1/h
+grid = np.append(np.arange(0.8, 4.0, h),[4.0]) # Patrick makes this a list
+multiplicities = []
+smallest_eigvals = []
+phi_vals = []
+DWR_phi_vals = []
+DWR_errors = []
+
+
+
+
+# Iterate over z grid
+for curr_z in grid:
+
+
+    z.assign(curr_z) # set z as current grid point
+
+    print(BLUE % f"Primal Solve:")
+
+
+    # Solve the primal problem!!
+
+
+    primal_nconv = primal_solver.solve() # solver the eigenproblem
+    min_eigval = primal_solver.eigenvalue(0) # the first (smallest) eigenvalue is what we desire
+    smallest_eigvals.append(min_eigval) # save the compute lambda_h 
+    uh = primal_solver.eigenfunction(0)[0] # pull the corresponding eigenfunction
+
+    # This should be nonnegative and real (self-adjoint).
+    # If it is negative, it may be because its really small or because of a geniune issue!
+    # So we will flag the user when the eigenvalue is negative.
+    neg_tol = 1e-8
+    if min_eigval < 0:
+      print(RED % f"WARNING - the min eigenval is negative: lambda_min = {min_eigval}")
+      if abs(min_eigval) <= neg_tol:
+        print(RED % f"Since the eigenvalue is small and negative, we set lambda_min = 0")
+        min_eigval = 0.0
+      else:
+        raise ValueError(RED % f"The eigenvalue is significantly negative and thats an issue so we quit!")
+
+    # check for multiplicity
+    print()
+    print('Primal eigvals:', sorted(primal_solver.eigenvalue(i) for i in range(primal_nconv)))
+    print()
+    mult_tol = 1e-2
+    mult_lst_primal = []
+    for val in sorted(primal_solver.eigenvalue(i) for i in range(primal_nconv)):
+        if abs(val - min_eigval) < mult_tol:
+            mult_lst_primal.append(val)
+    mult_count_primal = len(mult_lst_primal)
+
+
+    # Save all the eigenfunctions
+
+    z_dir_primal = f"recent_output/mixed/primal_problem_N-{N}/z-{float(curr_z):.6f}"
+    os.makedirs(z_dir_primal, exist_ok=True)
+
+    for k in range(primal_nconv):
+
+        # Pull the eigenfunction
+        eigfun = primal_solver.eigenfunction(k)[0]
+
+        # Normalize
+        Ek, Hk = split(eigfun)
+        m_val = assemble((inner(Ek, Ek) + inner(Hk, Hk))*dx)
+        eigfun.assign(eigfun / sqrt(m_val))
+
+        # Write mixed components separately
+        Eout, Hout = eigfun.subfunctions
+
+        Eout.rename(f"E_primal_{k}")
+        Hout.rename(f"H_primal_{k}")
+
+        VTKFile(f"{z_dir_primal}/primal_eig-{k}.pvd").write(Eout, Hout, time=float(curr_z))
+
+
+
+    # Compute Phi
+    phi = sqrt(min_eigval) # Our bound on distance to the spectrum - Phi_n = sqrt{lambda_min} >= dist(z, Sp(A))
+    phi_vals.append(phi)
+
+
+    # Normalize
+    Euh, Huh = split(uh)
+    m_val_uh = assemble((inner(Euh, Euh) + inner(Huh, Huh))*dx)
+    if abs(m_val_uh) < 1e-12:
+      print(RED % f"WARNING - m(u, u) is really small!!")
+    uh.assign(uh / sqrt(m_val_uh))
+
+
+
+    # Solve the enriched problem !!
+
+
+    print(BLUE % f"Enriched Solve:")
+
+    # lift the coarse solution to the enriched space as an inital guess
+    uh_lift = Function(Zp)
+    if hcurl:
+      uh_lift.project(uh, bcs=bcp)
+    else:
+      uh_lift.interpolate(uh, Zp)
+
+    # Set it as initial eigenspace guess
+    with uh_lift.dat.vec_ro as v:
+      enriched_solver.es.setInitialSpace([v.copy()])
+
+
+    # Solve enriched problem
+    enriched_nconv = enriched_solver.solve() # solve the eigenproblem
+    uph = enriched_solver.eigenfunction(0)[0]
+
+
+    # check for multiplicity
+    print()
+    print('Enriched eigvals:', sorted(enriched_solver.eigenvalue(i) for i in range(enriched_nconv)))
+    print()
+    mult_lst_enriched = []
+    min_eigval_enriched = enriched_solver.eigenvalue(0)
+    for val in sorted(enriched_solver.eigenvalue(i) for i in range(enriched_nconv)):
+        if abs(val - min_eigval_enriched) < mult_tol:
+            mult_lst_enriched.append(val)
+    mult_count_enriched = len(mult_lst_enriched)
+
+
+
+    # save all the enriched eigenfunctions
+    
+    z_dir_enriched = f"recent_output/mixed/enriched_problem_N-{N}/z-{float(curr_z):.6f}"
+    os.makedirs(z_dir_enriched, exist_ok=True)
+
+    for k in range(enriched_nconv):
+
+        # Pull the eigenfunction
+        eigfun = enriched_solver.eigenfunction(k)[0]
+
+        # Normalize
+        Ek, Hk = split(eigfun)
+        m_val = assemble((inner(Ek, Ek) + inner(Hk, Hk))*dx)
+        eigfun.assign(eigfun / sqrt(m_val))
+
+        # Write mixed components separately
+        Eout, Hout = eigfun.subfunctions
+
+        Eout.rename(f"E_primal_{k}")
+        Hout.rename(f"H_primal_{k}")
+
+        VTKFile(f"{z_dir_enriched}/eig-{k}.pvd").write(Eout, Hout, time=float(curr_z))
+
+
+    # Normalize
+    Eup, Hup = split(uph)
+    m_val_up = assemble((inner(Eup, Eup) + inner(Hup, Hup))*dx)
+    if abs(m_val_up) < 1e-12:
+      print(RED % f"WARNING - m(u, u) is really small!!")
+    uph.assign(uph / sqrt(m_val_up))
+
+
+    
+    
+    # Lets compare the multiplicities:
+
+    print(BLUE % f"primal multiplicity: {mult_count_primal}")
+    print(BLUE % f"enriched multiplicity: {mult_count_enriched}")
+
+    if mult_count_primal != mult_count_enriched:
+       print(RED % f"WARNING - the multiplicities of the primal and enriched problems are different!")
+
+    # The enriched multiplicity should be more accurate so we will take that one for the least squares problem
+    mult = mult_count_enriched
+    multiplicities.append(mult)
+    uh_basis_E = [primal_solver.eigenfunction(i)[0].subfunctions[0] for i in range(mult)]
+    uh_basis_H = [primal_solver.eigenfunction(i)[0].subfunctions[1] for i in range(mult)]
+    enriched_basis_E = [enriched_solver.eigenfunction(i)[0].subfunctions[0] for i in range(mult)]
+    enriched_basis_H = [enriched_solver.eigenfunction(i)[0].subfunctions[1] for i in range(mult)]
+
+
+    print(BLUE % f"We will take the enriched space multiplicity: {mult}")
+    
+
+
+
+    
+    # Create the Least squares problem 
+
+    # Assemble the K matrix 
+    K = np.zeros((mult, mult), dtype=complex)
+    for i in range(mult):
+        for j in range(mult):
+            # K[i, j] = assemble(inner(enriched_basis_E[i], enriched_basis_E[j])*dx) + assemble(inner(enriched_basis_H[i], enriched_basis_H[j])*dx) 
+            K[i, j] = assemble(inner(enriched_basis_E[j], enriched_basis_E[i])*dx) + assemble(inner(enriched_basis_H[j], enriched_basis_H[i])*dx) 
+
+    # Assemble the F vector
+    F = np.zeros((mult, 1), dtype=complex)
+    for i in range(mult):
+        F[i, 0] = assemble(inner(uh_basis_E[0], enriched_basis_E[i])*dx) + assemble(inner(uh_basis_H[0], enriched_basis_H[i])*dx)
+    
+    # Solve the least squares numpy problem
+    X = np.linalg.solve(K, F)
+
+    # Reassemble the needed enriched basis function
+    chosen_enriched = Function(Zp)
+    for i in range(mult):
+        basis_coeff = complex(X[i,0])
+        #import pdb; pdb.set_trace()
+        chosen_enriched.subfunctions[0].interpolate(chosen_enriched.subfunctions[0] + basis_coeff * enriched_basis_E[i])
+        chosen_enriched.subfunctions[1].interpolate(chosen_enriched.subfunctions[1] + basis_coeff * enriched_basis_H[i])
+
+    # Save this 
+    z_dir_ls = f"recent_output/mixed/least_squares_soln_N-{N}/z-{float(curr_z):.6f}"
+    os.makedirs(z_dir_ls, exist_ok=True)
+
+    # for k in range(enriched_nconv): # take this loop out later
+    #     # Normalize
+    #     Ek, Hk = split(chosen_enriched)
+    #     m_val = assemble((inner(Ek, Ek) + inner(Hk, Hk))*dx)
+    #     eigfun.assign(chosen_enriched / sqrt(m_val))
+    #     # Write mixed components separately
+    #     Eout, Hout = chosen_enriched.subfunctions
+    #     Eout.rename(f"E_primal_{k}")
+    #     Hout.rename(f"H_primal_{k}")
+    #     VTKFile(f"{z_dir_ls}/least_squares_eig-{k}.pvd").write(Eout, Hout, time=float(curr_z))
+
+   # normalize the chosen enriched function
+    E_chosen_up, H_chosen_up = split(chosen_enriched)
+    m_val_chosen_up = assemble((inner(E_chosen_up, E_chosen_up) + inner(H_chosen_up, H_chosen_up))*dx)
+    if abs(m_val_chosen_up) < 1e-12:
+      print(RED % f"WARNING - m(u, u) is really small!!")
+    chosen_enriched.assign(chosen_enriched / sqrt(m_val_chosen_up))
+    # Write mixed components separately
+    Eout, Hout = chosen_enriched.subfunctions
+    Eout.rename(f"E_primal_{k}")
+    Hout.rename(f"H_primal_{k}")
+    VTKFile(f"{z_dir_ls}/least_squares_result.pvd").write(Eout, Hout, time=float(curr_z))
+
+
+
+    # Error estimate !!
+
+
+    # Interpolate down to Z space
+    Ih_enriched = Function(Z)
+    if hcurl:
+      Ih_enriched.project(chosen_enriched, bcs=bc)
+    else:
+      Ih_enriched.interpolate(chosen_enriched, Z)
+
+    # Form wp
+    #wp = Function(Zp).assign(uph -  IhIh_up)
+    #wp = Function(Zp).assign(uph -  Ih_up) # wont work - ValueError: All functions in the expression must have the same element as the assignee
+    wp = Function(Zp).interpolate(chosen_enriched - Ih_enriched)
+
+    # Compute the primal residual
+    # Eh, Hh = split(uh_lift) #uh_lift is uh lifted to the enriched space - do I need to?
+    Eh, Hh = split(uh) 
+    Eph, Hph = split(wp) #pull out real part
+
+    a_primal = (
+          # A^* M^{-1} A terms
+          inner(rot_s(Eh), rot_s(Eph))*dx
+        + inner(rot_v(Hh), rot_v(Hph))*dx
+          # -2 z A terms
+        - 2 * z * inner(complex_unit*rot_v(Hh), Eph)*dx
+        + 2 * z * inner(complex_unit*rot_s(Eh), Hph)*dx
+          # |z|^2 M terms
+        + conj(z) * z * inner(Eh, Eph)*dx
+        + conj(z) * z * inner(Hh, Hph)*dx
+        )
+
+
+    b_primal = inner(Eh, Eph)*dx + inner(Hh, Hph)*dx
+
+    rho = a_primal - min_eigval * b_primal
+
+
+    # assemble
+    eig_error = assemble(rho)
+
+    # Check if really small
+    if abs(np.imag(eig_error)) > 0.0:
+      print(RED % f"WARNING - computed error has nontrivial imaginary part: {eig_error}")
+      print(RED % f"We will take its real part")
+      eig_error = np.real(eig_error)
+
+
+    # Use this to make phi better
+    improved_eig = min_eigval + eig_error
+    DWR_phi = sqrt(improved_eig)
+    DWR_phi_vals.append(DWR_phi)
+    DWR_errors.append(eig_error)
+
+
+    # Track the results
+    print(BLUE % f"z = {curr_z}: |dist(z, spectrum)| <= {phi}") # Patrick's output choice
+    print(BLUE % f"The error estimate is: {eig_error}")
+    print(BLUE % f"The improved eigenvalue is: {improved_eig}")
+    print()
+
+
+    # Set initial spaces for the next solves
+    primal_eigenfunctions = [primal_solver.eigenfunction(i)[0] for i in range(primal_nconv)] # pull out the converged eigenfunctions
+    primal_space = []
+    for eigenfunction in primal_eigenfunctions:
+        with eigenfunction.dat.vec_ro as v: # take the underlying PETSc object (read only)
+            primal_space.append(v.copy()) # copy the PETSc object into the eigenspace
+    primal_solver.es.setInitialSpace(primal_space) # set the eigenspace as the initial guess for the next iter (next grid point)
+
+
+
+
+# PLOTTING ROUTINE
+######################################################################################
+
+
+
+
+
+# desired eigenvalues
+exact_omega_squared = np.array([n**2 + m**2 for n in range(10) for m in range(10) if n**2 + m**2 <= 4**2])
+exact_omega = np.sqrt(exact_omega_squared)
+exact_omega = np.sort(exact_omega)
+# midpoints between neighbouring omega values
+midpoints = 0.5 * (exact_omega[1:] + exact_omega[:-1])
+
+# phi plot
+plt.plot(grid, phi_vals, linewidth=2, label = r"$\Phi_n(z, A)$")
+plt.plot(exact_omega, 0*exact_omega, 'ok', markersize=5, label = r'exact $\omega$')
+plt.plot(grid, DWR_phi_vals, label = r"DWR corrected $\Phi_n(z, A)$")
+os.makedirs("recent_output/mixed/", exist_ok=True)
+plt.xlabel(r"$z$")
+plt.title(rf"Approximations of $\Phi_n(z, A)$ and $\omega$ ($N = {N}$)")
+plt.legend()
+plt.savefig(f"recent_output/mixed/most_recent_phi_dwr_plot_{N=}.pdf")
+plt.close()
+
+# plot the multiplicities
+
+# plot the computed multiplicities
+plt.plot(grid, multiplicities, 'o', label = r"computed multiplicities")
+# plot the midpoints to indicate where the multiplicities should change
+plt.axvline(x=midpoints[0], color='r', linestyle='--', label = r"midpoints of exact $\omega$")
+for midpoint in midpoints[1:]:
+    plt.axvline(x=midpoint, color='r', linestyle='--')
+plt.xlabel(r"$z$")
+plt.title(rf"Computed multiplicities of the folded eigenvalues ($N = {N}$)")
+plt.legend()
+plt.savefig(f"recent_output/mixed/most_recent_multiplicities_plot_{N=}.pdf")
+plt.close()
+
+
